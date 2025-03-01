ae3c89ed315133cb121b17a8af2f950e86879acb
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-03-01 17:59:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="bg-sky-500 w-full h-screen">
+      <h1 className="flex text-4xl bg-sky-400 justify-center items-center h-screen">hellow world</h1>
+    </div>
+  );
+}
+
+export default App;
